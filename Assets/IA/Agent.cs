d03cbc5f262c4ffa1ace33a4ec190c93968cbe70
Jubi Jub <jubair.lekouer@epita.fr>
2024-03-24 00:37:39 +0100

    Ennemi

---
+++ Assets/IA/Agent.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Agent : MonoBehaviour
+{
+    private AgentAnimation agentAnimations;
+    private AgentMover agentMover;
+
+   
+
+    private Vector2 pointerInput, movementInput;
+
+    public Vector2 PointerInput { get => pointerInput; set => pointerInput = value; }
+    public Vector2 MovementInput { get => movementInput; set => movementInput = value; }
+
+    private void Update()
+    {
+        //pointerInput = GetPointerInput();
+        //movementInput = movement.action.ReadValue<Vector2>().normalized;
+
+        agentMover.MovementInput = MovementInput;
+   
+        AnimateCharacter();
+    }
+
+    public void PerformAttack()
+    {
+
+    }
+
+    private void Awake()
+    {
+        agentAnimations = GetComponentInChildren<AgentAnimation>();
+     
+        agentMover = GetComponent<AgentMover>();
+    }
+
+    private void AnimateCharacter()
+    {
+        Vector2 lookDirection = pointerInput - (Vector2)transform.position;
+        agentAnimations.RotateToPointer(lookDirection);
+        agentAnimations.PlayAnimation(MovementInput);
+    }
+}

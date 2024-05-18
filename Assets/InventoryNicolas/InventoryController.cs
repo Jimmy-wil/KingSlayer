fb9f2b798c0f6e55dc94822c48e176a7d461000f
Frisange57 <nboussert@pt.lu>
2024-05-18 22:32:31 +0200

    inventory update 1

--- Assets/InventoryNicolas/InventoryController.cs
+++ Assets/InventoryNicolas/InventoryController.cs
@@ -1,26 +1,41 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Netcode;
 using UnityEngine;
 
 public class InventoryController : MonoBehaviour
 {
     [SerializeField] private UIInvetoryPage inventoryUI;
-    private bool toggle;
+   // [SerializeField] private MouseFollower _mouseFollower;
+    private bool inventoryIsClosed;
 
     public int inventorySize = 10;
     void Start()
     {
         inventoryUI.InitializeInventoryUI(inventorySize);
+        inventoryIsClosed = false;
     }
 
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.I) == true)
+        if (Input.GetKeyDown(KeyCode.I))
         {
-            toggle = !toggle;
-            inventoryUI.gameObject.SetActive(toggle);
+            if (inventoryIsClosed)
+            {
+                inventoryUI.Show();
+                inventoryIsClosed = false;
+            }
+            else
+            {
+                inventoryUI.Hide();
+                inventoryIsClosed = true;
+            }
+            
+           // toggle = !toggle;
+           // inventoryUI.gameObject.SetActive(toggle);
+           // _mouseFollower.gameObject.SetActive(toggle);
         }
     }
 }

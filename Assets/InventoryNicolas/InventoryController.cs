00dc5e07eb50b188356ee80f187e820987daf3b1
Frisange57 <nboussert@pt.lu>
2024-06-09 21:27:01 +0200

    Inventaire descritption et swap

--- Assets/InventoryNicolas/InventoryController.cs
+++ Assets/InventoryNicolas/InventoryController.cs
@@ -1,53 +1,114 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Netcode;
 using UnityEngine;
 
 public class InventoryController : MonoBehaviour
 {
-   //Script a attacher au joueur pour qu il fonctionne !!!
+    [SerializeField] 
+    private UIInvetoryPage inventoryUI;
+   // [SerializeField] private MouseFollower _mouseFollower;
 
-   [SerializeField] private UIInvetoryPage inventoryUI;
    [SerializeField] private InventorySO inventoryData;
-   public int inventorySize = 10;
-   public bool inventoryIsClosed;
+   
+    private bool inventoryIsClosed;
+
+    public List<InventoryItem> initialItems = new List<InventoryItem>();
+
+    
     void Start()
-   {
-      inventoryUI.InitializeInventoryUI(inventorySize);
-      //inventoryData.Initialize();
-      inventoryIsClosed = false;
-   }
+    {
+        PrepareUI();
+        PrepareInventoryData();
+        inventoryIsClosed = false;
+    }
+
+    private void PrepareInventoryData()
+    {
+        inventoryData.Initialize();
+        inventoryData.OnInventoryUpdated += UpdateInventoryUI;
+        foreach (InventoryItem item in initialItems)
+        {
+            if (item.IsEmpty)
+                continue;
+            inventoryData.AddItem(item);
+            
+        }
+    }
+
+    private void UpdateInventoryUI(Dictionary<int, InventoryItem> inventoryState)
+    {
+        inventoryUI.ResetAllItems();
+        foreach (var item in inventoryState)
+        {
+            inventoryUI.UpdateData(item.Key,item.Value.item.ItemImage, item.Value.quantity);
+        }
+    }
+
+    private void PrepareUI()
+    {
+        inventoryUI.InitializeInventoryUI(inventoryData.Size);
+        this.inventoryUI.OnDescriptionRequested += HandleDescriptionRequest;
+        this.inventoryUI.OnSwapItems += HandleSwapItems;
+        this.inventoryUI.OnStartDragging += HandleDragging;
+        this.inventoryUI.OnItemActionRequested += HandleItemActionRequest;
+    }
+
+    private void HandleItemActionRequest(int itemIndex)
+    {
+       
+    }
+
+    private void HandleDragging(int itemIndex)
+    {
+        InventoryItem inventoryItem = inventoryData.GetItemAt(itemIndex);
+        if(inventoryItem.IsEmpty)
+            return;
+        inventoryUI.CreateDraggedItem(inventoryItem.item.ItemImage, inventoryItem.quantity);
+    }
+
+    private void HandleSwapItems(int itemIndex1, int itemIndex2)
+    {
+        inventoryData.SwapItems(itemIndex1, itemIndex2);
+    }
+
+    private void HandleDescriptionRequest(int itemIndex)
+    {
+        InventoryItem inventoryItem = inventoryData.GetItemAt(itemIndex);
+        if (inventoryItem.IsEmpty)
+        {
+            inventoryUI.ResetSelection();
+            return;
+        }
+           
+        ItemSO item = inventoryItem.item;
+        inventoryUI.UpdateDescription(itemIndex, item.ItemImage, item.name, item.Description);
+    }
 
 
     void Update()
-   {
-      if (Input.GetKeyDown(KeyCode.I))
-      {
-         if (inventoryIsClosed == true)
-         {
-            inventoryUI.Show();
-           // foreach (var item in inventoryData.GetCurrentItemState())
-           // {
-           //    inventoryUI.UpdateData(item.Key,item.Value.item.ItemImage, item.Value.quantity);
-          //  }
-
-            inventoryIsClosed = false;
-         }
-         else
-         {
-            inventoryUI.Hide();
-            inventoryIsClosed = true;
-         }
-        
-      }
-     
-   }
-   
-
-   
-    
-    
-    
-    
-    
+    {
+        if (Input.GetKeyDown(KeyCode.I))
+        {
+            if (inventoryIsClosed)
+            {
+                inventoryUI.Show();
+                foreach (var item in inventoryData.GetCurrentItemState())
+                {
+                    inventoryUI.UpdateData(item.Key, item.Value.item.ItemImage,item.Value.quantity);
+                }
+                inventoryIsClosed = false;
+            }
+            else
+            {
+                inventoryUI.Hide();
+                inventoryIsClosed = true;
+            }
+            
+           // toggle = !toggle;
+           // inventoryUI.gameObject.SetActive(toggle);
+           // _mouseFollower.gameObject.SetActive(toggle);
+        }
+    }
 }
